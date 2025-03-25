e4309a03cc0a2f425514d9e0329e4fd123a52cc4
achwak <belliliachwak@gmail.com>
2025-03-25 23:47:40 +0100

    base ya rabii

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -16,6 +16,9 @@ class Image
     #[ORM\ManyToOne(inversedBy: 'image')]
     private ?Product $product = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +32,18 @@ class Image
     public function setProduct(?Product $product): static
     {
         $this->product = $product;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
 
         return $this;
     }
