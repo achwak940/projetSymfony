1b4aa21d7d60be357f56c9a4ade8c14a752710b3
achwak <belliliachwak@gmail.com>
2025-04-07 14:19:57 +0100

    liset

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 final class ProductController extends AbstractController
 {
     #[Route('/product', name: 'app_product')]
@@ -14,5 +16,25 @@ final class ProductController extends AbstractController
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
         ]);
+    }
+    #[Route('/produits', name:'recipe.index')]
+    public function listeProduits(Request $request,ProductRepository $repository,EntityManagerInterface $en) : Response
+    {
+        $produits= $repository->findAll();
+      //$recipes[0]->setTitle('salade');
+    //   $en->flush();
+    /*$recipe = new Recipe();
+    $recipe->setTitle('Barbe-papa')
+           ->setSlug('brabe-papa')
+           ->setContent('Mettez de sucre')
+           ->setDuration(2)
+           ->setCreatedAt(new \DateTime())
+           ->setUpdatedAt(new \DateTime());
+    $en->flush();*/
+        return $this->render('product/listproduits.html.twig',[
+            'produits'=>
+            $produits
+        ]
+    );
     }
 }
